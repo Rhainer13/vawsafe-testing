7dab758225534dedf979f882d573defe34a485b5
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-07-30 16:20:32 +0800

    aaa

--- vawsafe/desk_officer/forms.py
+++ vawsafe/desk_officer/forms.py
@@ -14,8 +14,15 @@ class IncidentInformationForm(ModelForm):
     class Meta:
         model = IncidentInformation
         exclude = ['victim_survivor']
+        widgets = {
+            'incident_date': forms.DateInput(attrs={'type': 'date'}),
+            'incident_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
 
 class AllegedPerpetratorInformationForm(ModelForm):
     class Meta:
         model = AllegedPerpetratorInformation
         exclude = ['victim_survivor']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+        }
